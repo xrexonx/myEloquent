4633817e07c1b66f046871505b642777fde63805
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2016-02-05 20:18:39 -0800

    02052015 commits

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'config/database.php';
+
+
+require 'start.php';
---
+++ models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent {
+
+	protected $fillable = ['email'];
+	public $timestamps = false;
+
+}
