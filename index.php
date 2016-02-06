7c9451bb0bf9016ec3670af3f519c1dbe5a79d9e
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2016-02-05 22:54:44 -0800

    Add sample view file using blade class

--- index.php
+++ index.php
@@ -3,5 +3,15 @@
 require 'vendor/autoload.php';
 require 'config/database.php';
 
+// require 'start.php';
 
-require 'start.php';
+use Philo\Blade\Blade;
+
+$views = __DIR__ . '/views';
+$cache = __DIR__ . '/cache';
+
+$blade = new Blade($views, $cache);
+echo $blade
+		->view()
+		->make('hello')
+		->render();
